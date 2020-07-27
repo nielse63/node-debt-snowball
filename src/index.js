ffae0fb4bf7ed7e3e7389e5bde7818cb9a673e30
Erik Nielsen <erik@312development.com>
2020-07-27 01:55:29 -0500

    refactored classes to commonjs exports

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import DebtSnowball from './DebtSnowball';
-
-export default (accounts, additionalPayment) => {
-  const snowball = new DebtSnowball(accounts, additionalPayment);
-  return snowball.createPaymentPlan();
-};
