507fe5eebc36affcbadd863c9390552219eb7426
Erik Nielsen <erik@312development.com>
2024-04-26 13:47:59 -0400

    updated esm config

---
+++ examples/index.mjs
@@ -0,0 +1,20 @@
+import snowball from '../dist/esm/index.js';
+
+const accounts = [
+  {
+    name: 'Credit Card',
+    interest: 14.99,
+    balance: 1000,
+    minPayment: 75,
+  },
+  {
+    name: 'Student Loan',
+    interest: 4.75,
+    balance: 7500,
+    minPayment: 150,
+  },
+];
+const additionalPayment = 100;
+
+const repaymentPlan = snowball(accounts, additionalPayment);
+console.log('repaymentPlan', repaymentPlan);
